3d892358ecffbd3ff27f103289e7b26b01a86755
mohammedtaufeeqfb-ux <mohammedtaufeeqfb@gmail.com>
2026-07-03 10:33:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello from Jenkins CI/CD Pipeline!, webhook");
+    res.end("Hello from Jenkins CI/CD Pipeline!, webhook V2");
 });
 
 server.listen(3000, () => {

426276613855c7f8e009de4ae17e7dddfde2ed8b
Ubuntu <ubuntu@ip-172-31-14-255.ec2.internal>
2026-07-03 04:43:26 +0000

    Updated app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello from Jenkins CI/CD Pipeline!");
+    res.end("Hello from Jenkins CI/CD Pipeline!, webhook");
 });
 
 server.listen(3000, () => {
